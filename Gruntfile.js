876b4a165e1ae3cdb24daaa70b3557e9a4717f26
Bastian Krol <bastian.krol@web.de>
2014-11-02 18:22:35 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+/*
+ * grunt-version-assets
+ * https://github.com/basti1302/grunt-version-assets
+ *
+ * Copyright (c) 2014 Bastian Krol
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    versioning: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!'
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'versioning', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/version_assets.js
@@ -0,0 +1,67 @@
+/*
+ * grunt-version-assets
+ * https://github.com/basti1302/grunt-version-assets
+ *
+ * Copyright (c) 2014 Bastian Krol
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var Version = require("node-version-assets");
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask(
+  'versioning',
+  'Rename static assets and update references',
+  function() {
+    var done = this.async();
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      grepFiles: [],
+      keepOriginal: true,
+    });
+
+    Object.keys(options).forEach(function(key) {
+      grunt.log.debug('Option: ' + key + ': ' + JSON.stringify(options[key]));
+    });
+
+    var grepFilesExpanded = grunt.file.expandMapping(options.grepFiles);
+
+    grunt.log.debug('**GREPFILES**');
+    var grepFiles = [];
+    grepFilesExpanded.forEach(function(file) {
+      grepFiles.push(file.src[0]);
+      grunt.log.debug(file.src[0]);
+    });
+
+    // Iterate over all specified file groups.
+    grunt.log.debug('**ASSETS**');
+    var assets = [];
+    this.filesSrc.forEach(function(file) {
+      assets.push(file);
+      grunt.log.debug(file);
+    });
+
+    var versioner = new Version({
+      assets: assets,
+      grepFiles: grepFiles,
+      keepOriginal: options.keepOriginal,
+    });
+
+    versioner.run(function(err) {
+      if (err) {
+        done(false);
+      } else {
+        done();
+      }
+    });
+
+  });
+
+};
